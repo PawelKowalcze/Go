9acd2ddf406f3f3a7ebcc61bcd84f2a2cc221c1b
PawelKowalcze <kowalczepawel2004@gmail.com>
2024-07-31 22:34:58 +0200

    First GO files

---
+++ Calculator.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi, this is your Calculator")
+	fmt.Println("Please, choose your operation")
+
+	var operationType int
+	fmt.Scanf("%d", &operationType)
+	
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!!")
+
+	Number := 15
+	fmt.Printf("%v \n", Number)
+
+}
